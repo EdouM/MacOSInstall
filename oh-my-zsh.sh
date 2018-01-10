2d2c357867e0db11d44fc5ab0f9e9470434cf993
Malet <EdouM@users.noreply.github.com>
2018-01-10 23:38:13 +0100

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -5,6 +5,7 @@ source  ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+echo "Installing fonts"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
