4b23007fa74a9e8b45f298bc0237c4afc95cc76f
Malet <EdouM@users.noreply.github.com>
2018-01-10 23:00:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ defaults write com.apple.Dock showhidden -bool YES
 defaults write com.apple.dock expose-animation-duration -float 0.12
 # Check for updates daily
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-# Key repeat
-defaults write NSGlobalDomain KeyRepeat -int 0
-defaults write NSGlobalDomain InitialKeyRepeat -int 0
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
