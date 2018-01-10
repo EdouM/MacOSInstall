f0534d2f37845f42d481b70f2dd4ae30ed4bb9c4
Malet <EdouM@users.noreply.github.com>
2018-01-10 23:31:03 +0100

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,6 +1,6 @@
 
 # ZSH
-mv .zshrc  ~/.zshrc
+cp .zshrc  ~/.zshrc
 source  ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
