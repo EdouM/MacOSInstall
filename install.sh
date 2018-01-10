dc8d248fc8a192d843f00ca50562a720b6d37d08
Malet <EdouM@users.noreply.github.com>
2018-01-10 23:05:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,18 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 # Save screenshots to Downloads
 defaults write com.apple.screencapture location -string "~/Downloads"
+# Expand save panel by default.
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+# Expand print panel by default.
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+# Set keyboard repeat rate to "damn fast".
+defaults write NSGlobalDomain KeyRepeat -int 2
+# Set a shorter delay until key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+# Disable sound effects when changing volume
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -integer 0
+# Use a dark menu bar / dock
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
 # atom
 apm install sync-settings
