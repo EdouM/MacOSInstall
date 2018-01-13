c13683525457316d0ea1a5c950c543c1b6548d9c
Malet <EdouM@users.noreply.github.com>
2018-01-13 18:45:41 +0100

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -2,7 +2,6 @@
 # ZSH
 cp .zshrc  ~/.zshrc
 source  ~/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
